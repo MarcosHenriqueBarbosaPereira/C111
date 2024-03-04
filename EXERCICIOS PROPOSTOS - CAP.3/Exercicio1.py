28c1193288fee191fd2726ca56d1a5b6dffed176
MarcosHenriqueBarbosaPereira <marcos.h@gec.inatel.br>
2024-03-04 17:22:10 -0300

    EXERCICIOS PROPOSTOS - CAP.3

---
+++ EXERCICIOS PROPOSTOS - CAP.3/Exercicio1.py
@@ -0,0 +1,15 @@
+colocacao = ["Argentina", "França", "Croácia", "Marrocos", "Holanda"]
+
+#A
+print(colocacao[0:3])
+
+#B
+print(colocacao[3:])
+
+#C 
+alfabetico = colocacao
+alfabetico.sort()
+print(alfabetico)
+
+#D Foi mal, não entendo de futebol pra saber algum campeonato com Barcelona (coloquei a Copa de 2022) :)
+print(colocacao.index("Marrocos"))
